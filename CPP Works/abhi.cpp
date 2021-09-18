cdbd08f4ede77053847d3aa60d66178db0bba327
Deepak Gupta <spycaptain3@gmail.com>
2021-09-18 10:32:34 +0530

    d

---
+++ CPP Works/abhi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout<<"Enter the value of a: \n";
+    cin>>a;
+   
+
+    if(a==5){
+        cout<<"It is equal to 5\n";
+        goto abhi;
+        
+    }
+    else{
+        cout<<"It is not equal to 5\n";
+    }
+    cout<<"Please enter the number darling \n";
+   abhi:
+    cout<<"You are my dill dubaa \n";
+    return 0;
+}
